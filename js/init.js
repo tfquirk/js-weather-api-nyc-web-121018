2046a89579d1d49576515ef7abe2c4f19e042fde
aalexander3 <aralx73@gmail.com>
2018-07-19 18:38:46 -0400

    reset key

--- js/init.js
+++ js/init.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ctx = document.getElementById("NYCWeatherChart").getContext("2d")
-  // var API_KEY = "Go get an API key";
-  const API_KEY = "b032709bcff50e56c7017d8d8ef2234e";
+  var API_KEY = "Go get an API key";
   const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
   const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently?exclude=minutely?exclude=daily";
 
