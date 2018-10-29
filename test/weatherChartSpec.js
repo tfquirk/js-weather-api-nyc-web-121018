a45a7424a8b2c95ff951cd555a363f8e7894e8e5
Arren Alexander <aralx73@gmail.com>
2018-10-29 16:46:38 -0400

    Update weatherChartSpec.js

--- test/weatherChartSpec.js
+++ test/weatherChartSpec.js
@@ -59,7 +59,7 @@ describe('weatherChart', () => {
 
     it('fetches the create fork api', () => {
       const ctx = document.getElementById("NYCWeatherChart").getContext("2d");
-      var API_KEY = "Go get an API key";
+      const API_KEY = "Go get an API key";
       const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
       const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently,minutely,daily";
 
