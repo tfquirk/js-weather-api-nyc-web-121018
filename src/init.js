2090979c1bb81463fb141ef23bcee670b8290d46
andrew cohn <andrewcohn0@gmail.com>
2019-01-14 11:04:05 -0500

    rm api key

--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   const ctx = document.querySelector("#NYCWeatherChart").getContext("2d")
-  const API_KEY = "276e16bdd042419e96c094e549e7014c";
+  const API_KEY = "YOUR API KEY";
   const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
   const DARK_SKY_BASE = "https://api.darksky.net/"
   const URL = `${CORS_WRAPPER}${DARK_SKY_BASE}/forecast/${API_KEY}/40.7127,-74.0059?exclude=currently,minutely,daily`
